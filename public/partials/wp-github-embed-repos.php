f1218f84bbdb965e26f1f823db0471641c0134a2
Tyler Kanz <tylerkanz@gmail.com>
2021-08-24 16:59:28 -0400

    Shortcode additions

--- public/partials/wp-github-embed-repos.php
+++ public/partials/wp-github-embed-repos.php
@@ -1,9 +1,21 @@
 <?php
 add_shortcode('gh-repo', 'gh_repo_func');
-
 function gh_repo_func()
 {
     $response = fetch_from_github('https://api.github.com/repos/tylerkanz/tk-theme/git/trees/main');
-    var_dump(json_decode($response['body']));
+    $tree = json_decode($response['body']);
+
+    $directories_files = array();
+    foreach ($tree->tree as $tree_object) {
+        if ($tree_object->type == 'tree') {
+            $directories_files[] = array('folder'=>$tree_object->path);
+        }
+    }
+    foreach ($tree->tree as $tree_object) {
+        if ($tree_object->type != 'tree') {
+            $directories_files[] = array('file' => $tree_object->path);
+        }
+    }
+    var_dump($directories_files);
 }
 ?>
