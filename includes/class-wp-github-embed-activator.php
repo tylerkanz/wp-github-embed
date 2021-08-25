f66d7d7a532c4cf05a06d5266a721cc67c52859e
Tyler Kanz <tylerkanz@gmail.com>
2021-08-25 12:38:18 -0400

    Create WP Table for meta

--- includes/class-wp-github-embed-activator.php
+++ includes/class-wp-github-embed-activator.php
@@ -31,6 +31,19 @@ class Wp_Github_Embed_Activator {
 	 */
 	public static function activate() {
 
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' ); 
+		global $wpdb;
+		$sql = "CREATE TABLE  ". $wpdb->prefix . "git_embed_meta (
+			meta_id bigint(20) NOT NULL auto_increment,
+			meta_key varchar(255) default NULL,
+			user varchar(255) default NULL,
+			repo varchar(255) default NULL,
+			meta_value longtext,
+			PRIMARY KEY  (`meta_id`)
+		  )";
+
+		maybe_create_table( $wpdb->prefix . 'git_embed_meta', $sql );
+
 	}
 
 }
